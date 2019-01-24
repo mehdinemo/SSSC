2f8d190358640b30dbb0a57cc46b3bbf686cf8af
mehdinemo <37899528+mehdinemo@users.noreply.github.com>
2019-01-24 12:59:25 +0330

    Add files via upload

--- SparseMoltiply.m
+++ SparseMoltiply.m
@@ -7,7 +7,12 @@ for i=1:N
     h(i) = SD(i)*x(i);
     [r, c] = ind2sub(M, find(SM(:,1:2)==i));
     for j=1:length(r)
-        h(i) = h(i)+SL(r(j),)
+        if c==1
+            h(i) = h(i) - (SM(r(j),3)*x(SM(r(j),2)));
+        end
+        if c==2
+            h(i) = h(i) - (SM(r(j),3)*x(SM(r(j),1)));
+        end
     end
 end
 
